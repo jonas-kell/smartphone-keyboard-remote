b2db23d3077daed40d129bce61b0ae2bbf37e9a0
Jonas Kell <jonas-kell@web.de>
2025-06-08 00:38:45 +0200

    refactor: raw mode

--- client/src/components/BasicUsage.vue
+++ client/src/components/BasicUsage.vue
@@ -20,6 +20,9 @@
         @focusin="liveTypeFocus = true"
         @focusout="liveTypeFocus = false"
     />
+    <br />
+    <input type="checkbox" id="raw" v-model="liveTypeRaw" />
+    <label for="raw">Send Raw Keycodes</label>
 </template>
 
 <script setup lang="ts">
@@ -32,6 +35,7 @@
     const passwordField = ref(false);
 
     const liveTypeFocus = ref(false);
+    const liveTypeRaw = ref(false);
 
     function preventAndSend(event: Event) {
         let eventTyped = event as KeyboardEvent;
@@ -44,6 +48,7 @@
         if (codeToSend == "NumpadEnter") {
             codeToSend = "Super"; // Numpad Enter sends super, as we can not reliably detect that otherwise
         }
+        codeToSend += "--" + (liveTypeRaw.value ? "raw" : "key") + "--" + eventTyped.key;
         mainStore.checkedEncryptedBackendCommunication(method, codeToSend, "ack_key");
     }
     watch(liveTypeFocus, () => {
